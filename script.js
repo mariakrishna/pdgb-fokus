3f71b97a608c9dcd3ac9483ffcf28733fd52c9b2
mariakrishna <mkadonai@gmail.com>
2024-05-14 21:58:15 -0300

    .

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const pauseSound = new Audio('./sons/pause.mp3');
 const playSound = new Audio('./sons/play.wav');
 const finishSound = new Audio('./sons/beep.mp3');
 
-let elapsedTimeInSeconds = 30;
+let elapsedTimeInSeconds = 1500;
 let intervalId = null;
 music.loop = true
 
